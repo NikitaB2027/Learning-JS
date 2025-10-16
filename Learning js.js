70f5362de7baed19e1cda6468fbdeb5d6ed6b6aa
Nikita Baruah <nikita_baruah27@milton.edu>
2025-10-16 09:49:49 -0400

    Made the false and true number thing

---
+++ Learning js.js
@@ -0,0 +1,9 @@
+console.log("I am printing");
+let x =Math.floor(Math.random()*1000);
+if (x%7&&9==0){
+    console.log(x);
+    console.log("Success");
+} else{
+    console.log(x);
+    console.log("Failure");
+}
