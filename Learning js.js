127f7a1023e971c0bb7d5097ce13773dc1eac1a2
Nikita Baruah <nikita_baruah27@milton.edu>
2025-10-20 20:46:06 -0400

    addition equation- #3

--- Learning js.js
+++ Learning js.js
@@ -7,3 +7,14 @@ if (x%7&&9==0){
     console.log(x);
     console.log("Failure");
 }
+let t= Math.floor(Math.random()*150);
+console.log("temp="+t);
+let temp= (t<60) ? "too cold" : (t>90 ? "too hot" : "just right");
+console.log(temp);
+
+function add(a,b){
+    return a+b;
+}
+let result= add(Math.floor(Math.random()*10),Math.floor(Math.random()*10));
+console.log("Result= "+result);
+
