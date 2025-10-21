80f606ad0d5076b45346b637387260bc982b93b7
Nikita Baruah <nikita_baruah27@milton.edu>
2025-10-21 12:29:30 -0400

    working on loop- #6

--- Learning js.js
+++ Learning js.js
@@ -18,3 +18,21 @@ function add(a,b){
 let result= add(Math.floor(Math.random()*10),Math.floor(Math.random()*10));
 console.log("Result= "+result);
 
+let myFunction= (a,b)=> a+b;
+let arrresult= myFunction(Math.floor(Math.random()*50),Math.floor(Math.random()*50));
+console.log("ArrResult= "+arrresult);
+
+const pplITalkedToToday= ["Remy", "Panshul", "Crystal", "Matt", "Aru", "Ime", "Mr.Goodman", "Ines", "Mr. Fuller", "Rhys", "Mx. Bradford", "Rowan", "Isa", "Lisa", "Ezra" ];
+let randomElement= pplITalkedToToday[Math.floor(Math.random()*15)];
+console.log("Array: "+pplITalkedToToday.length+", "+ pplITalkedToToday[0]+", "+pplITalkedToToday[14]+", "+randomElement+", "+pplITalkedToToday.sort()+", "+pplITalkedToToday.push("Katie")+", "+pplITalkedToToday.push(35));
+
+const numbers= [];
+for(let i=0; i<10; i++){
+    let myMinMax= (min, max)=> Math.floor(Math.random()*(max-min+1))+min;
+    let randomNum= myMinMax(29, 87);
+    numbers.push(randomNum);
+}
+console.log(numbers);
+for(let j=0; j<numbers.length; j++){
+    
+}
